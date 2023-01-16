e55239506eed6712f52568cb84b7e8a1972f090e
rishabkumar7 <rishabkumar7@gmail.com>
2023-01-15 23:20:40 -0500

    Added Lambda function and HTML for views

--- website/index.js
+++ website/index.js
@@ -55,3 +55,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+    let response = await fetch(
+        "https://wwjcx7tyxrbjmbkf3vc3teo3mu0qrvhq.lambda-url.ca-central-1.on.aws/"
+    );
+    let data = await response.json();
+    counter.innerHTML = `👀 Views: ${data}`;
+}
+updateCounter();
